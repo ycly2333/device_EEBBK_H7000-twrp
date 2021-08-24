8e548e56e1f6c5eb68d7b0da15c092ffb23ab880
Liam Luo <lonelyjskj@gmail.com>
2021-08-25 00:29:57 +0800

    H7000: Corrected really brightness path

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -83,7 +83,7 @@ TW_MAX_BRIGHTNESS := 4095
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
-TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
+TW_BRIGHTNESS_PATH := /sys/devices/soc/c900000.qcom,mdss_mdp/c900000.qcom,mdss_mdp:qcom,mdss_fb_primary/leds/lcd-backlight/brightness
 
 # exFAT FS Support
 TW_INCLUDE_FUSE_EXFAT := true
