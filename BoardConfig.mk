f8e8752f601d743bbe7f4f7ef2fe50e03fc64446
Liam Luo <lonelyjskj@gmail.com>
2021-09-03 06:42:02 +0800

    H7000: Fixed brightness control.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,11 +79,11 @@ TARGET_RECOVERY_QCOM_RTC_FIX := true
 TW_EXCLUDE_DEFAULT_USB_INIT := true
 TW_INCLUDE_CRYPTO := true
 TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_MAX_BRIGHTNESS := 4095
+TW_MAX_BRIGHTNESS := 255
 TW_SCREEN_BLANK_ON_BOOT := true
 TW_THEME := portrait_hdpi
 TW_USE_TOOLBOX := true
-TW_BRIGHTNESS_PATH := /sys/devices/soc/c900000.qcom,mdss_mdp/c900000.qcom,mdss_mdp:qcom,mdss_fb_primary/leds/lcd-backlight/brightness
+TW_BRIGHTNESS_PATH := /sys/class/leds/lcd-backlight/brightness
 
 # Language
 TW_EXTRA_LANGUAGES := true
